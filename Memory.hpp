42af2a92e47ca22b0e3081b02c87d946c5745202
Xander <xander@kali.xgv>
2024-11-16 20:34:22 -0700

    restores the issue

--- Memory.hpp
+++ Memory.hpp
@@ -1,4 +1,28 @@
 #ifndef Memory_hpp
 #define Memory_hpp
+class Memory{
+    int memorySize;
+    unsigned char* MainMemory; 
 
-#endif
+    Memory(int size){
+        this.memorySize = size;
+        MainMemory = [size];
+        for(int i = 0;i<size,i++){
+            MainMemory[i] = i%255;
+        }
+    }
+
+    unsigned char getByte(unsigned long address){
+        return(this.MainMemory[address = size]);
+    }
+
+    void setBytes(unsigned long address){
+        this.MainMemory[address] = 1;
+    }
+
+    void displayBits();
+};
+
+
+
+#endifint
