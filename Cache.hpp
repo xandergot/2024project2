74a487a5f032727695e762c77e69e6c80840f10e
duAnnabella <166453450+duAnnabella@users.noreply.github.com>
2024-11-17 13:34:23 -0700

    Cache.hpp

--- Cache.hpp
+++ Cache.hpp
@@ -1,6 +1,33 @@
-#ifndef Cache_hpp
-#define Cache_hpp
+#ifndef Cache_hpp // checks if Cache_hpp is defined.
+// If Cache_hpp is defined, the program skip to after the #endif
+#define Cache_hpp // if not defined, define Cache_hpp
 
 
+class Cache{
+    private:
+        int size;
+        int associativity;
+        int blockSize;
 
-#endif
+    public:
+        Cache(int size, int associativity, int blockSize);
+        
+        // Getters
+        int getSize();
+        int getAssociativity();
+        int getBlockSize();
+
+        // Setters
+        void setSize(int size);
+        void setAssociativity(int associativity);
+        void setBlockSize(int blockSize);
+
+        // Core operations
+        void read();
+        void write();
+        void display();
+   
+};
+
+#endif // closes the ifndef block
+// The program would continue here
