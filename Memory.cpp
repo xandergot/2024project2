62b21a672c5efdddf7671c9c90c1a56c62bd095e
xander <xandergot@gmail.com>
2024-11-21 16:51:12 -0500

    fix merge issue

--- Memory.cpp
+++ Memory.cpp
@@ -1,27 +1,33 @@
 #include "Memory.hpp"
+#include <iostream> // io stream allows you use cout and cin
 
-
-Memory::Memory(int size){
-        this->memorySize = size;
-        MainMemory = [size];
-        for(int i = 0;i<size,i++){
-            MainMemory[i] = i%255;
-        }
+Memory::Memory(int size) {
+    this->memorySize = size;
+    MainMemory = new unsigned char[size]; // Correctly allocate memory
+    for (int i = 0; i < size; i++) { // Use semicolon instead of comma
+        MainMemory[i] = i % 255;
+    }
 }
 
-void Memory::setBytes(unsigned long address){
+Memory::~Memory() {
+    delete[] MainMemory; // Free allocated memory
+}
+
+void Memory::setBytes(unsigned long address) {
     this->MainMemory[address] = 1;
 }
 
-unsigned char Memory::getByte(unsigned long address){
-    return(this->MainMemory[address = size]);
+unsigned char Memory::getByte(unsigned long address) {
+    return this->MainMemory[address]; // Correct array access
 }
 
-
-int Memory::getMemorySize(){
-    return(this->memorySize);
+int Memory::getMemorySize() {
+    return this->memorySize; // Use arrow operator for member access
 }
 
-void Memory::displayBits(){
-    
+void Memory::displayBits() {
+    for (int i = 0; i < memorySize; i++) {
+        std::bitset<8> bits(MainMemory[i]);
+        std::cout << "Address " << i << ": " << bits << std::endl;
+    }
 }
