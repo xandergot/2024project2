ceb6f90d421bdffc7d2aa767c9f57ac5b5bdd187
xander <xandergot@gmail.com>
2024-11-21 15:05:05 -0500

    added the hpp files

--- Block.hpp
+++ Block.hpp
@@ -1,4 +1,30 @@
 #ifndef Block_hpp
 #define Block_hpp
 
+class Block {
+private:
+    unsigned long tag;
+    bool valid;
+    int data;  // is this an int?
+
+public:
+    // Constructor
+    Block() : tag(0), valid(false), data(0) {}
+
+    // Getters
+    unsigned long getTag() const { return tag; }
+    bool isValid() const { return valid; }
+    int getData() const { return data; }
+
+    // Setters
+    void setTag(unsigned long t) { tag = t; }
+    void setValid(bool v) { valid = v; }
+    void setData(int d) { data = d; }
+
+    // Core functions
+    int readData() const { return data; }
+    void writeData(int d) { data = d; }
+};
+
+
 #endif
--- Set.hpp
+++ Set.hpp
@@ -1,4 +1,23 @@
 #ifndef Set_hpp
 #define Set_hpp
 
+#include "Block.hpp"
+
+class Set {
+    private:
+        int associativity;
+        Block* blocks;
+
+    public:
+        // Constructor
+        Set(int associativity, int blockSize);
+
+        // Core functions
+        Block* findBlock(unsigned long address);
+        Block* loadBlock(unsigned long address);
+        void evictBlock();
+        void display();
+
+};
+
 #endif
