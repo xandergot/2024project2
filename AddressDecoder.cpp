eab1b6c9264a280e0f1c99171b9c7e1e5ff640a8
duAnnabella <166453450+duAnnabella@users.noreply.github.com>
2024-11-21 14:40:51 -0700

    Added decode address to AddressDecoder

--- AddressDecoder.cpp
+++ AddressDecoder.cpp
@@ -14,3 +14,18 @@ AddressDecoder::AddressDecoder(int numSets, int blockSize) {
     // Calculate the number of bits needed for the tag
     this->tagBits = 32 - blockOffsetBits - setIndexBits; // Assuming 32-bit addresses
 }
+
+AddressComponents AddressDecoder::decodeAddress(int address) const {
+    AddressComponents components;
+
+    // Extract the block offset bits
+    components.blockOffset = address & (blockSize - 1);
+
+    // Extract the set index bits
+    components.setIndex = (address >> blockOffsetBits) & (numSets - 1);
+
+    // Extract the tag bits
+    components.tag = address >> (blockOffsetBits + setIndexBits);
+
+    return components;
+}
